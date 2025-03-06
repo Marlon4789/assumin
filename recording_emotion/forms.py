34bab4203cfdfe26c043b026dd03134a559a1d1f
Marlon4789 <cuartasm844@gmail.com>
2025-03-05 21:26:12 -0500

    logo y font style

--- recording_emotion/forms.py
+++ recording_emotion/forms.py
@@ -7,6 +7,11 @@ class RecordingEmotionForm(forms.ModelForm):
         fields = ['emotion', 'description_day']
         widgets = {
             'emotion': forms.Select(attrs={'class': 'form-select border-dark focus-ring focus-ring-secondary py-1 px-2 text-decoration-none'}),
-            'description_day': forms.Textarea(attrs={'class': 'form-control border-dark focus-ring focus-ring-secondary py-1 px-2 text-decoration-none', 'rows': 10}),
+            'description_day': forms.Textarea(attrs={'class': 'form-control border-dark focus-ring focus-ring-secondary py-1 px-2 text-decoration-none', 'rows': 13, 'placeholder': '¿Cómo estuvo tu día?'}),
             # 'discovery': forms.Textarea(attrs={'class': 'form-control border-dark border-1', 'rows': 2}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Agrega una opción vacía con el texto deseado al principio de las choices.
+        self.fields['emotion'].choices = [('', '¿Califica cómo estuvo tu día?')] + list(self.fields['emotion'].choices)
